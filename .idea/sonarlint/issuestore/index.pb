76477fd5c7c304b094622abd928caa6a49220d33
Debnatp <Debnatp@labcorp.com>
2021-11-13 12:20:56 -0500

    First commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+n
+>src/main/java/com/lca/phoenix/version/control/Application.java,b\7\b7ed778348a44be68e23124f3b8d746901014ac8
---
+++ src/main/java/com/lca/phoenix/version/control/Application.java
@@ -0,0 +1,17 @@
+/*****************************************************
+ * Copyright (c) by Laboratory Corporation of America *
+ * All Rights Reserved.                               *
+ ******************************************************/
+
+package com.lca.phoenix.version.control;
+
+/**
+ * Debnatp
+ * 11/13/2021
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
